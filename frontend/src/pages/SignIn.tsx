b762afc32770cd5067377a1ca23959137c447e0b
MrJujek <julian.dworzycki@gmail.com>
2023-06-06 23:17:02 +0200

    style: :lipstick: sign in styling

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,18 +1,14 @@
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input, Typography } from 'antd';
 
 function SignIn() {
-  const [login, setLogin] = useState("")
-  const [password, setPassword] = useState("")
+  const { Text } = Typography;
+
   const { signIn, user } = useAuth();
   const navigate = useNavigate();
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-
-    signIn(login, password);
-  }
 
   useEffect(() => {
     if (user) {
@@ -20,20 +16,54 @@ function SignIn() {
     }
   }, [user, navigate]);
 
+  const onFinish = (values: { login: string, password: string }) => {
+    console.log('Success:', values);
+
+    signIn(values.login, values.password);
+  };
+
   return (
     <>
-      <div>
+      <div className="signIn">
         <h1>Sign in</h1>
-        <form className="signIn" action="POST" onSubmit={(e) => handleSubmit(e)}>
-          <label htmlFor="nickName">Nickname or email:</label>
-          <input type="text" id="nickName" value={login} onChange={(e) => { setLogin(e.target.value) }} />
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" onChange={(e) => { setPassword(e.target.value) }} />
-          <button className="signInButton" type="submit">Sign in</button>
-        </form>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="login"
+            rules={[{ required: true, message: 'Please input your nickname or email!' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Nickname or email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
 
-        <p>Don't have an account?</p>
-        <Link to="/signup">Sign Up</Link>
+          {/* <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item> */}
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+        <div className="signInLink">
+          <Text>Don't have an account?</Text>
+          <Button type="primary"><Link to="/signup">Sign Up</Link></Button>
+        </div>
+
       </div>
     </>
   )
