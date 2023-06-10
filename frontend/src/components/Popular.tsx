babbfcdf73b4b15d61c36bca69bf6441d98f8ba9
MrJujek <julian.dworzycki@gmail.com>
2023-06-11 00:02:07 +0200

    feat: :sparkles: show 10 most popular tags

--- frontend/src/components/Popular.tsx
+++ frontend/src/components/Popular.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
-import { Typography } from 'antd'
+import { useEffect, useContext, useState } from 'react'
+import { Typography } from 'antd';
+import UrlContext from '@/contexts/UrlContext';
 
 function Popular() {
     const { Title } = Typography
+    const { url } = useContext(UrlContext);
+
+    const [popularTags, setPopularTags] = useState<{ id: number, name: string, popularity: number }[]>([])
+
+    useEffect(() => {
+        loadPopularTags();
+    }, [])
+
+    async function loadPopularTags() {
+        const response = await fetch(url + "/tags", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+
+            let top10 = data.slice(0);
+            top10.sort(function (a: { id: number, name: string, popularity: number }, b: { id: number, name: string, popularity: number }) {
+                return b.popularity - a.popularity;
+            });
+            top10.splice(10);
+
+            setPopularTags(top10.filter((tag: { id: number, name: string, popularity: number }) => tag.popularity > 0))
+        }
+    }
+
     return (
         <aside className='popular'>
             <Title level={2}>Popular</Title>
+
+            {popularTags.length > 0 &&
+                popularTags.map((tag) => {
+                    return (
+                        <div>
+                            #{tag.name} - {tag.popularity}
+                        </div>
+                    )
+                })
+            }
         </aside>
     )
 }
